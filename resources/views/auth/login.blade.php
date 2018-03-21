8361c885884be2ca03d4537f9fdbeab785f113e6
Carlos <carlosferrerhernandez10@gmail.com>
2018-03-21 17:57:20 -0500

    Update login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,33 +5,29 @@
 <section  class="newsletter_section">
     <div class="container">
         <div class="row">
-            <div class="col-md-offset-2 text-center col-xs-12 col-md-8" data-aos="fade-up">
-                <h1 class="newsletter-title">Iniciar sesión</h1>
-                <div class="newsletter_form">
+            <div class="text-center col-xs-12 col-md-6 col-md-offset-3" data-aos="fade-up">
+                <div class="text-center">
+                    <img src="#" alt="" width="72" height="72">
+                </div>
+                <h1 class="newsletter-title">AUTOHERNANDEZ DE LA COSTA</h1>
+                <div class="newsletter_form text-center">
                     <form>
-                        <div class="input-group input-group-lg newsletter col-md-12 col-sm-12">
+                        <div class="form-label-group newsletter col-md-12 col-sm-12">
                             <input type="email" name="email" class="form-control" placeholder="Correo electronico">
                         </div>
-                        <br>
-                        <div class="input-group input-group-lg newsletter col-md-12 col-sm-12">
+                        <div class="form-label-group newsletter col-md-12 col-sm-12">
                             <input type="password" name="password" class="form-control" placeholder="Contraseña">
                         </div>
-
                         <div class="text-center">
-                            <span class="input-group-btn text-center">
-                                <button type="submit" name="subscribe" class="btn-newsletter-bg">Suscribete</button>
+                            <span class="input-group-btn">
+                                <button type="submit" name="subscribe" class="btn-newsletter-bg">INGRESAR</button>
                             </span> 
-                        </div>
-
-                        <div id="mce-responses">
-                            <div class="response" id="mce-error-response" style="display:none"></div>
-                            <div class="response" id="mce-success-response" style="display:none"></div>
                         </div>
                     </form>
                 </div>
-            </div>END COL
-        </div>END ROW
-    </div>END CONTAINER
+            </div>
+        </div>
+    </div>
 </section>
 
 @endsection
